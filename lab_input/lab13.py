fd30b64c65540542703e89b4fe718d136623076a
Peg <davontapeagler21@gmail.com>
2024-06-26 18:09:13 +0000

    lab 13

---
+++ lab_input/lab13.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+    #collect string input from user
+   # user_input1 = input("Hello, what is your name?", user_input2 = input ("Do you know what day it is?")
+
+    #collect string week day from user
+    #user_input2 = input("Do you know what day it is?")
+
+    #print() users name and day of the week
+   # print("Hello:" + user_input1, "Happy:" + user_input2) 
+    
+
+#!/usr/bin/env python3
+
+# collect string input from user
+user_input1 = input("Hello, what is your name?")
+user_input2 = input("Do you know what day it is?")
+
+# print() users name and day of the week
+print("Hello: " + user_input1 + ", Happy: " + user_input2)
